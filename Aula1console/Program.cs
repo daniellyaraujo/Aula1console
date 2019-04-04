5a4ca547fb7b67961bef10c9ecf37b91030f133b
Danielly Araujo <daraujo@iauditeden.onmicrosoft.com>
2019-04-04 11:29:20 -0300

    Add project files.

---
+++ Aula1console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula1console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Condicao;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Como voce esta?");
+            Condicao = Console.ReadLine();
+
+            Console.WriteLine("A sua condição é " + Condicao);
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
